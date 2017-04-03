a4b0548a45d27173e5514915b11f788f8f2a189c
Felipe <Felipe@P000167>
2017-04-03 11:25:25 -0300

    Teste

---
+++ AppDB/AppDB/Views/CredentialProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDB.Views
+{
+    class CredentialProvider : ViewModels.Services.ICredentialProvider
+    {
+    }
+}
